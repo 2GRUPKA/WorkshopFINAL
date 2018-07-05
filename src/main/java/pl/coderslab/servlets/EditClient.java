e4504772ed8d5bfefd11bd94b91e63ed8d6d3375
Karolina Gajewska <38051417+karussia@users.noreply.github.com>
2018-07-05 20:52:38 +0200

    Add files via upload

---
+++ src/main/java/pl/coderslab/servlets/EditClient.java
@@ -0,0 +1,39 @@
+package pl.coderslab.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Date;
+import pl.coderslab.dao.clientsDao;
+
+
+@WebServlet(name = "EditClient",urlPatterns = "/EditClient")
+public class EditClient extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String idStr = request.getParameter("id");
+        String name = request.getParameter("name");
+        String lastName = request.getParameter("lastName");
+        String date = request.getParameter("birthDate");
+        Date birthDate = Date.valueOf(date);
+        int id;
+        try {
+            id = Integer.parseInt(idStr);
+            clientsDao ClientsDao = clientsDao.loadbyId(id);
+            ClientsDao.editClients();
+            ClientsDao.saveClients();
+
+        } catch (Exception e) {
+            response.getWriter().append("Could not edit the client");
+        }
+    }
+
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
